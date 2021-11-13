9915e55ed43d1612aa830e507cb15a0853dd00f3
kazumaneet <mekbibtare@gmail.com>
2021-11-13 18:37:50 +0300

    removed the child elements instead of removing the listeners and refactored most of the code

--- script/script.js
+++ script/script.js
@@ -1,22 +1,38 @@
 const resetButton = document.querySelector('#reset');
 const container = document.querySelector('#container');
-container.style.gridTemplateColumns = `repeat(16,1fr)`;
+let squareNumb = parseInt(askForGridNUmb());
+createGrid(squareNumb);
 
-for (let i = 0; i < 256;i++){
-    let square = document.createElement('div');
-    square.classList.add('square');
-    container.append(square);
+function createGrid(squareNumb){
+    container.style.gridTemplateColumns = `repeat(${squareNumb},1fr)`;
+    for (let i = 0; i < squareNumb * squareNumb;i++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container.append(square);
+    }
+    const squares = document.querySelectorAll('.square');
+    sqaureAddListener(squares);
 }
 
-const squares = document.querySelectorAll('.square');
-
-squares.forEach(square =>{
-    square.addEventListener('mouseover', function(e){
-        e.target.classList.add('square-active');
-    })
-})
-resetButton.addEventListener('click',()=>{
+function sqaureAddListener(squares){
     squares.forEach(square =>{
-    square.classList.remove('square-active');
-})})
+        square.addEventListener('mouseover', function(e){
+            e.target.classList.add('square-active');
+        })
+    })
+}
+
+function askForGridNUmb(){
+    return window.prompt("how many squares per side would you like: ");
+}
+
+resetButton.addEventListener('click', ()=>{
+    removeSquares();
+    createGrid(askForGridNUmb());
+})
+
+function removeSquares(){
+    while(container.firstElementChild)
+        container.removeChild(container.firstElementChild);
+}
 
